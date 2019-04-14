4ac322cd3167a5292a36c61f7d186d566deedd74
Sike Dong 001828931 <dong.si@husky.neu.edu>
2019-04-13 20:44:23 -0400

    server for bakery app

---
+++ server/server.js
@@ -0,0 +1,32 @@
+let express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'), //created model loading here
+    bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.connect('mongodb+srv://SikeMongo:Dsk930123@cluster0-a3ke7.mongodb.net/test?retryWrites=true', {
+    useNewUrlParser: true
+});
+mongoose.Promise = global.Promise;
+
+//Adding body parser for handling request and response objects.
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+//Enabling CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//Initialize app
+let initApp = require('./api/app');
+initApp(app);
+
+// Launch app to listen to specified port
+app.listen(port);
+console.log('Users RESTful API server started on: ' + port);
