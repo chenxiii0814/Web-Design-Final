72b24e37dc4aee9f82f567e6a64aaa6de5d2231c
chenxiii0814 <chen.xi3@husky.neu.edu>
2019-04-19 16:08:03 -0500

    finish signin and sign up without database by cx

--- bakery-app/src/app/register/register.component.ts
+++ bakery-app/src/app/register/register.component.ts
@@ -12,4 +12,38 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
   }
 
+  signUp(){
+    let username = (<HTMLInputElement>document.getElementById("username")).value;
+    let password = (<HTMLInputElement>document.getElementById("password")).value;
+    let phone = (<HTMLInputElement>document.getElementById("phone")).value;
+    let email = (<HTMLInputElement>document.getElementById("email")).value;
+
+    //check whether username or password is null.
+    let errorInfo = "";
+    if (username == "") {
+      errorInfo = "Username cannot be null!\n";
+    }
+    if (password == "") {
+      errorInfo = "Password cannot be null!\n";
+    }
+    if (phone == "") {
+      errorInfo = "Phone number cannot be null!\n";
+    }
+    if (email == "") {
+      errorInfo = "Email cannot be null!\n";
+    }
+    if (errorInfo == "") {
+      alert("Hi "+username+"! Enjoy your cake!");
+    }
+    else {
+      alert(errorInfo);
+      return false;
+    }
+
+    //connect database here!
+
+
+    console.log("username: "+username+"\npassword: "+password+"\nphone: "+phone+"\nemail: "+email);
+  }
+
 }
