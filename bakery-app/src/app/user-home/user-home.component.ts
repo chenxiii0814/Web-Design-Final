5a46c7876195e440792c01aa2708fe6644e128e1
DanqiuZhang <zhang.danq@husky.neu.edu>
2019-04-21 16:26:02 -0400

    create login controller

--- bakery-app/src/app/user-home/user-home.component.ts
+++ bakery-app/src/app/user-home/user-home.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-user-home',
   template: `
     <p>
-      user-home works!
+      userhome working!
+      {{username}}
     </p>
   `,
   styles: []
 })
 export class UserHomeComponent implements OnInit {
+  username:String;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params)=>this.username=params.username);
+
   }
 
 }
